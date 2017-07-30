90f4154b350a5629bfe90646380537ba57e7960a
Administrator <644724460@qq.com>
2017-07-30 21:28:33 +0800

    ok

---
+++ movie_news_web/src/main/java/org/movie/dao/IAdminDao.java
@@ -0,0 +1,17 @@
+package org.movie.dao;
+
+import org.movie.model.Admin;
+
+public interface IAdminDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    Admin selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+}
